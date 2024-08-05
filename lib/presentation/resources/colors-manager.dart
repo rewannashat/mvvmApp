3e3641b3451d15669d683e440bbbfadb0faef637
rewannashat <mrrwan55@gmail.com>
2024-08-05 11:59:20 +0300

    first push

---
+++ lib/presentation/resources/colors-manager.dart
@@ -0,0 +1,30 @@
+
+import 'dart:ui';
+import 'package:flutter/material.dart';
+class ColorsManager {
+
+  // special color app :
+  static const Color buttonColor = Color(0xff71CBFF);
+  static const Color drawerColor = Color(0xff38B6FF);
+  static const Color backGroundPhotoColor = Color (0xffD9E4FF);
+  static const Color iconColor = Color(0xff38B6FF);
+  static const Color titleTextColor = Color(0xff71CBFF);
+  static const Color baseTextColor = Color(0xff151940);
+  static const Color staticTextColor = Color(0xff677294); //Skip , Continue ,seeAll
+  static const Color messageTextColor = Color(0xff5771F9); //ForgetPass
+  static const Color backGroundPhoto2Color = Color(0xffEBF5FF);
+  static const Color iconBottomBarColor = Color(0xff858EA9);
+  static const Color disableButtonColor = Color(0xff898A8D);
+  static const Color disableFavColor = Color(0xff777EA5);
+  static const Color dollarColor = Color(0xff0EBE7E);
+  static const Color buttonCategoryColor = Color(0xff161540);
+  static const Color iconHelpColor = Color(0xffFFAD00);
+
+  // default color in any app :
+  static const Color redColor = Color(0xffFF0000);
+  static const Color starColor = Color(0xffF6D060);
+  static const Color greyColor = Colors.grey;
+  static const Color blackColor = Colors.black;
+  static const Color whiteColor = Colors.white;
+
+}
---
+++ lib/presentation/resources/font-manager.dart
@@ -0,0 +1,24 @@
+
+import 'dart:ui';
+
+class FontManager {
+  static const String fontFamilyTitle = 'Poly' ;
+  static const String fontFamilyApp = 'Inter';
+}
+
+class FontWightManager {
+  static const FontWeight fontWeightLight = FontWeight.w400;
+  static const FontWeight fontWeightRegular = FontWeight.w500;
+  static const FontWeight fontWeightMedium = FontWeight.w600;
+  static const FontWeight fontWeightSemiBold = FontWeight.w700;
+  static const FontWeight fontWeightBold = FontWeight.w800;
+}
+
+class FontSize {
+  static const double s10 = 10.0 ;
+  static const double s12 = 12.0;
+  static const double s15 = 15.0;
+  static const double s18 = 18.0;
+  static const double s20 = 20.0;
+  static const double s30 = 30.0;
+}
